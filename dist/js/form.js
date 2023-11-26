754683245549f27060ec039836d81eecdd99efa7
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2023-11-26 13:11:11 -0500

    chore: split js and css into seperate files

---
+++ dist/js/form.js
@@ -0,0 +1,21 @@
+// Initialize the agent at application startup.
+var fpPromise = FingerprintJS.load()
+var visitorId = "";
+
+// Analyze the visitor when necessary.
+fpPromise
+    .then(fp => fp.get())
+    .then(result => fillForm(result.visitorId));
+
+// create function to handle displaying the id
+function fillForm(id) {
+    visitorId = id;
+    document.getElementById("visitorId").innerHTML = "Visitor ID: " + visitorId;
+    document.getElementById("visitor_id").value = visitorId;
+    // get the ip address
+    fetch('https://api.ipify.org?format=json')
+        .then(results => results.json())
+        .then(data => {
+            document.getElementById("ip").value = data.ip;
+        });
+}
