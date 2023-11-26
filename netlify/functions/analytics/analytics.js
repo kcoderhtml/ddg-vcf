fd06cd802fe3f68e30a69f3a0101ea75902ac410
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2023-11-25 22:17:06 -0500

    feat: Add email checking and VCard functionality

--- netlify/functions/analytics/analytics.js
+++ netlify/functions/analytics/analytics.js
@@ -21,26 +21,84 @@ function urlEncodedFormDataToJson(formDataString) {
 	return formData;
 }
 
+function genEmail() {
+	return "lolz@duck.com";
+}
+
+async function checkForEmail(visit_id) {
+	try {
+		const query = q.Get(q.Match(q.Index("email_by_visit_id"), visit_id));
+		const response = await faunaClient.query(query);
+
+		if (response.data) {
+			const email = response.data.email;
+			console.log(`Email found: ${email}`);
+			return email;
+		} else {
+			console.log("No document found for the given visit ID");
+			return null;
+		}
+	} catch (error) {
+		console.error("Error:", error);
+		throw error; // rethrow the error if needed
+	}
+}
+
+async function genVCard(visit_id) {
+	try {
+		const email = await checkForEmail(visit_id);
+
+		if (email) {
+			console.log(`Handling email: ${email}`);
+			const vcard = `BEGIN:VCARD
+		  VERSION:3.0
+		  N:Lastname;Firstname;;;
+		  EMAIL: ${email}
+		  END:VCARD`;
+
+			return vcard;
+		} else {
+			console.log("Email not found");
+			return null;
+		}
+	} catch (error) {
+		console.error("Error during VCard generation:", error);
+		throw error; // rethrow the error if needed
+	}
+}
+
 const handler = async (event) => {
 	try {
 		if (event.httpMethod !== "POST") {
 			return { statusCode: 500, body: "POST OR BUST!" };
 		}
 
-    jsonizedForm = urlEncodedFormDataToJson(event.body)
-    jsonizedForm['date'] = new Date().toISOString()
+		jsonizedForm = urlEncodedFormDataToJson(event.body);
+		jsonizedForm["date"] = new Date().toISOString();
 
 		// convert the post request to json
 		const data = { data: jsonizedForm };
 
 		// save the data to fauna
-		const req = await faunaClient.query(q.Create(q.Ref("classes/usage"), data));
-		console.log(req);
+		await faunaClient.query(q.Create(q.Ref("classes/usage"), data));
 
-		return {
-			statusCode: 200,
-			body: JSON.stringify({ message: "Successfully added email!" }),
-		};
+		vcard = await genVCard(jsonizedForm["visitor_id"]);
+
+		if (vcard) {
+			console.log(`Handling VCard: ${vcard}`);
+
+			return {
+				statusCode: 200,
+				headers: {
+					"Content-Type": "text/vcard",
+					"Content-Disposition": 'attachment; filename="vcard.vcf"',
+				},
+				body: vcard,
+			};
+		} else {
+			console.log("VCard not generated");
+			return { statusCode: 500, body: "VCard not generated" };
+		}
 	} catch (error) {
 		return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
 	}
